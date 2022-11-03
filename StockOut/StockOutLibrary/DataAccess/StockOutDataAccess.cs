5a04cc104663886b5892903f9ff5294aad111e1a
Rey <95774734+reinhard24@users.noreply.github.com>
2022-11-04 06:17:10 +0700

    Add files via upload

---
+++ StockOut/StockOutLibrary/DataAccess/StockOutDataAccess.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StockOutLibrary.Models;
+
+namespace StockOutLibrary.DataAccess
+{
+    public partial class StockOutDataAccess : DbContext
+    {
+        public StockOutDataAccess()
+        {
+        }
+
+        public StockOutDataAccess(DbContextOptions<StockOutDataAccess> options) :
+            base(options)
+        {
+
+        }
+
+        public virtual DbSet<StockOutModel> stockouts { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Name=hybrid");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StockOutModel>(entity =>
+            {
+                entity.ToTable("stockouts");
+
+                entity.Property(e => e.stockout_id).HasColumnName("stockout_id");
+
+                entity.Property(e => e.product_id).HasColumnName("product_id");
+
+                entity.Property(e => e.stockout_qty).HasColumnName("stockout_qty");
+
+                entity.Property(e => e.out_date).HasColumnName("out_date");
+
+                entity.Property(e => e.notes).HasColumnName("notes");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    }
+}
---
+++ StockOut/StockOutLibrary/Models/StockOutModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockOutLibrary.Models
+{
+    public class StockOutModel
+    {
+        [Key]
+        public int stockout_id { get; set; }
+        public int? product_id { get; set; }
+        public int? stockout_qty { get; set; }
+        public DateTime? out_date { get; set; }
+        public string? notes { get; set; }
+
+    }
+}
