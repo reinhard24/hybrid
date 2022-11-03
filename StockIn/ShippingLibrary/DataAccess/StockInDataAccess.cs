7c269641301a8a1e33ded73e583247a5a2d09b59
Rey <95774734+reinhard24@users.noreply.github.com>
2022-11-04 05:58:49 +0700

    Add files via upload

---
+++ StockIn/ShippingLibrary/DataAccess/StockInDataAccess.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockInLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockInLibrary.DataAccess
+{
+    public partial class StockInDataAccess : DbContext
+    {
+        public StockInDataAccess()
+        {
+        }
+
+        public StockInDataAccess(DbContextOptions<StockInDataAccess> options) :
+            base(options)
+        {
+
+        }
+
+        public virtual DbSet<StockInModel> stockins { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Name=hybrid");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StockInModel>(entity =>
+            {
+                entity.ToTable("stockins");
+
+                entity.Property(e => e.stockin_id).HasColumnName("stockin_id");
+
+                entity.Property(e => e.product_id).HasColumnName("product_id");
+
+                entity.Property(e => e.stockin_qty).HasColumnName("stockin_qty");
+
+                entity.Property(e => e.entry_date).HasColumnName("entry_date");
+
+                entity.Property(e => e.notes).HasColumnName("notes");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    }
+}
---
+++ StockIn/ShippingLibrary/Models/StockInModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace StockInLibrary.Models
+{
+    public class StockInModel
+    {
+        [Key]
+        public int stockin_id { get; set; }
+        public int? product_id { get; set; }
+        public int? stockin_qty { get; set; }
+        public DateTime? entry_date { get; set; }
+        public string? notes { get; set; }
+
+        //[JsonIgnore]
+        //public virtual products? product { get; set; }
+    }
+}
