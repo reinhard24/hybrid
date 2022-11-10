cac58981ba1cac988d999bc7bd03ee4037a526c2
Rey <95774734+reinhard24@users.noreply.github.com>
2022-11-10 12:26:14 +0700

    Add files via upload

---
+++ Login/LoginLibrary/DataAccess/LoginDataAccess.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoginLibrary.Models;
+
+namespace LoginLibrary.DataAccess
+{
+    public partial class LoginDataAccess : DbContext
+    {
+        public LoginDataAccess()
+        {
+        }
+
+        public LoginDataAccess(DbContextOptions<LoginDataAccess> options) :
+            base(options)
+        {
+
+        }
+
+        public virtual DbSet<LoginModel> logins { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Name=hybrid");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserDetailsModel>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.Property(e => e.user_id).HasColumnName("user_id");
+                entity.Property(e => e.user_name).HasColumnName("user_name");
+                entity.Property(e => e.email).HasColumnName("email");
+                entity.Property(e => e.user_password).HasColumnName("user_password");
+                entity.Property(e => e.phone).HasColumnName("phone");
+                entity.Property(e => e.address).HasColumnName("address");
+                entity.Property(e => e.role_id).HasColumnName("role_id");
+
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Login/LoginLibrary/Models/LoginModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginLibrary.Models
+{
+    [Keyless]
+    public class LoginModel
+    {
+        public string user_name { get; set; }
+        public string user_password { get; set; }
+    }
+
+    [Keyless]
+    public class Result
+    {
+        public bool result { get; set; }
+        public string message { get; set; }
+    }
+}
